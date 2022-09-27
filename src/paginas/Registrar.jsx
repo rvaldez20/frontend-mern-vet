be6af4880e9b0cd42dabf60cbd8d13a34b0409a9
centinela1024tb <centinela1024tb@gmail.com>
2022-09-27 16:04:34 -0500

    [ADD] En registrar se agregan los state para cada campo
    => nombre | email | password | repetirPassword

--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 const Registrar = () => {
+
+  const [nombre, setNombre] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [repetirPassword, setRepetirPassword] = useState('')
+
   return (
     <>
       <div>
@@ -20,6 +27,8 @@ const Registrar = () => {
               type="text"
               placeholder="Tu Nombre"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={nombre}
+              onChange={e => setNombre(e.target.value)}
             />
           </div>
 
@@ -30,6 +39,8 @@ const Registrar = () => {
               type="email"
               placeholder="Email de Registro"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
             />
           </div>
 
@@ -39,6 +50,8 @@ const Registrar = () => {
               type="password"
               placeholder="Tu Password"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
             />
           </div>
 
@@ -48,6 +61,8 @@ const Registrar = () => {
               type="password"
               placeholder="Repite tu Password"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={repetirPassword}
+              onChange={e => setRepetirPassword(e.target.value)}
             />
           </div>
 
