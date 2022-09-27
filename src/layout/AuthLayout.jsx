ff8fc323c3d6270bd52609a553b19f2c0091fcaf
centinela1024tb <centinela1024tb@gmail.com>
2022-09-27 15:14:15 -0500

    [ADD] Se empíeza a agregar estilos al login
    => Se definen 2 columnas (title:left | form:right)
    => Se agrega el formulario para hacer login se solicita login y password

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -2,10 +2,10 @@ import { Outlet } from 'react-router-dom'
 
 const AuthLayout = () => {
   return (
-    <>
-      <h1>Administrador de Pacientes de Veterinaria</h1>
-
-      <Outlet />
+    <>      
+      <main className="container mx-auto md:grid md:grid-cols-2 gap-10 mt-12 p-5">
+        <Outlet />
+      </main>
     </>
   )
 }
