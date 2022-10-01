5cd136cba01eb3f9998b3412f47144dd70f52d11
centinela1024tb <centinela1024tb@gmail.com>
2022-10-01 13:12:16 -0500

    [CONFIG] Se agrega y configura las routas protegidas
    => RutasProtegidas mediente el uso del AuthContex
    => Extramos los datos deusuario logueado del useAuth
    => En Auth Provider se tuvo que egregar el state caragndo
    => Mientras no cargue se mustra mensaje Cargando...
    => Una vez que cargando es false verificamos que el objeto tenga la propiedad _id

--- src/layout/RutaProtegida.jsx
+++ src/layout/RutaProtegida.jsx
@@ -1,10 +1,19 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Navigate } from "react-router-dom"
+import useAuth from '../hooks/useAuth'
 
 const RutaProtegida = () => {
+
+  // extraemos info del user que hizo login del useAuth (Auth Context)
+  const { auth, cargando } = useAuth();
+  console.log(auth)
+  console.log(cargando)
+
+  if(cargando) return 'cargando...'
+
   return (
     <>
       <h1>Es es una ruta protegida</h1>
-      <Outlet />
+      {auth?._id ? <Outlet /> : <Navigate to="/" />}
     </>
   )
 }
