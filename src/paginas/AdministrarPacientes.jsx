80111289100f8a28892e08622ec989d8d72a0e71
centinela1024tb <centinela1024tb@gmail.com>
2022-10-04 13:27:04 -0500

    [ADD] Se crea el formulario para agregar un paciente y oculta/muestra en base al tamaño del dispositivo

--- src/paginas/AdministrarPacientes.jsx
+++ src/paginas/AdministrarPacientes.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+
 import Formulario from "../components/Formulario"
 import ListadoPacientes from "../components/ListadoPacientes"
 
 
 const AdministrarPacientes = () => {
+
+  const [mostrarFormulario, setmostrarFormulario] = useState(false)
+
   return (
     <div className="flex flex-col md:flex-row">
-      <div className="md:w-1/2 lg:w-2/5">
+      <button
+        type="button"
+        className="bg-indigo-600 text-white font-bold uppercase rounded-md mx-10 p-3 mb-10 md:hidden"
+        onClick={() => setmostrarFormulario(!mostrarFormulario)}
+      >
+        {mostrarFormulario ? 'Ocultar Formualrio' : 'Mostrar formulario'}       
+      </button>
+      <div className={`${mostrarFormulario ? 'block' : 'hidden' } md:block md:w-1/2 lg:w-2/5`}>
         <Formulario />
       </div>
 
