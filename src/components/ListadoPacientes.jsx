27ffcf79f14f6724e7ba23f01f80052f43fefaa9
centinela1024tb <centinela1024tb@gmail.com>
2022-10-04 15:31:14 -0500

    [ADD] Se empieza a implementar el mostrar los pacientes de forma condicional (si hay o no hay pacientes)

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,32 @@
+import usePacientes from '../hooks/usePacientes';
 
 const ListadoPacientes = () => {
+
+  const { pacientes } = usePacientes();
+  console.log(pacientes)
+
   return (
-    <h1>ListadoPacientes</h1>
+    <>
+      { pacientes.length ? 
+      (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {''}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
+        </>
+      ) : 
+      (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {''}
+            <span className="text-indigo-600 font-bold">y apareceran en este lugar</span>
+          </p>
+        </>
+      )}
+    </>
   )
 }
 
