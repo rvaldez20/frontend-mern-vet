be12ee348059b8b15d0c0c303ad88d8c394c937e
centinela1024tb <centinela1024tb@gmail.com>
2022-10-05 17:21:50 -0500

    [ADD] Se hace la maquetación del formulario para editar perfíl

--- src/paginas/EditarPerfil.jsx
+++ src/paginas/EditarPerfil.jsx
@@ -10,6 +10,55 @@ const EditarPerfil = () => {
       <p className="text-xl mt-5 mb-10 text-center">Modifica tu {' '} 
         <span className="text-indigo-700 font-bold">Perfíl Aquí</span>
       </p>
+
+      <div className="flex justify-center">
+        <div className="w-full md:w-1/2 bg-white shadow rounded-lg p-5">
+          <form>
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">Nombre</label>
+              <input 
+                type="text" 
+                className="border bg-gray-50 w-full p-2 mt-3 rounded-lg"
+                name="nombre"
+              />
+            </div>
+
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">Sitio Web</label>
+              <input 
+                type="text" 
+                className="border bg-gray-50 w-full p-2 mt-3 rounded-lg"
+                name="web"
+              />
+            </div>
+
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">Teléfono</label>
+              <input 
+                type="text" 
+                className="border bg-gray-50 w-full p-2 mt-3 rounded-lg"
+                name="telefono"
+              />
+            </div>
+
+            <div className="my-3">
+              <label className="uppercase font-bold text-gray-600">Email</label>
+              <input 
+                type="text" 
+                className="border bg-gray-50 w-full p-2 mt-3 rounded-lg"
+                name="email"
+              />
+            </div>
+
+            <input 
+              type="submit" 
+              value="Guardar Cambios"
+              className="bg-indigo-700 px-10 py-3 font-bold text-white w-full rounded-lg uppercase mt-5"
+            />
+          </form>
+        </div>
+      </div>
+
     </>
   )
 }
